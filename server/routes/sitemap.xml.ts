bb6bcdb487bf44c6a00c1f31aa54d15adf78b121
AliAslani1234 <ali_aslaniii10@yahoo.com>
2025-08-02 01:19:03 -0700

    fix

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,31 +1,36 @@
-import { SitemapStream, streamToPromise } from 'sitemap'
-import { setHeader } from 'h3'
+import { SitemapStream, streamToPromise } from "sitemap";
+import { setHeader } from "h3";
 
 export default defineEventHandler(async (event) => {
-  const { serverQueryContent } = await import('#content/server') // <-- dynamic import
+  const { serverQueryContent } = await import("#content/server"); // <-- dynamic import
 
   // Fetch all content pages
-  const docs = await serverQueryContent(event).find()
+  const docs = await serverQueryContent(event).find();
 
-  const sitemap = new SitemapStream({ hostname: 'https://canvas.hrcd.fr' })
+  const sitemap = new SitemapStream({
+    hostname: "https://ali-aslani-dev.vercel.app",
+  });
 
   // Add content pages
+  // Add content pages
   for (const doc of docs) {
-    sitemap.write({
-      url: doc._path,
-      changefreq: 'daily',
-    })
+    for (const locale of ["fa", "en"]) {
+      sitemap.write({
+        url: `/${locale}${doc._path === "/" ? "" : doc._path}`,
+        changefreq: "daily",
+      });
+    }
   }
 
   // Add static routes
-  const staticRoutes = ['/', '/about', '/contact', '/works', '/writing']
+  const staticRoutes = ["/", "/about", "/contact", "/works", "/writing"];
   for (const route of staticRoutes) {
-    sitemap.write({ url: route, changefreq: 'daily' })
+    sitemap.write({ url: route, changefreq: "daily" });
   }
 
-  sitemap.end()
-  const xml = await streamToPromise(sitemap)
+  sitemap.end();
+  const xml = await streamToPromise(sitemap);
 
-  setHeader(event, 'Content-Type', 'application/xml')
-  return xml.toString()
-})
+  setHeader(event, "Content-Type", "application/xml");
+  return xml.toString();
+});
